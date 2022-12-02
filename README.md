18c7a74acbdc9a46ff547fc037d311b7c28433e0
Vincent Menant <vincent.menant@ucaya.com>
2022-12-02 11:14:17 +0100

    Advent of code 2022 - Day one - First puzzle

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# adventofcode-rust
+# AdventofCode 2022 in Rust
+
+Just my adventures in Rust discovery with Advent of Code 2022
---
+++ day 1/puzzle-one/isis-milann.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+fn main() {
+    println("########## # ############################ ##### ####################### #")
+    println("##### Advent of code 2022 - Day one - First puzzle #####")
+    println("########## # ############################ ##### ####################### #")
+
+    let mut most_hungry_elf : i32 = 0;
+    let mut current_elf_food_needs : i32 = 0;
+
+    let file_path = "inputs/one";
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the input file");
+
+    let lines = contents.lines();
+
+    for elf in lines {
+        if elf.is_empty() {
+            if current_elf_food_needs > most_hungry_elf {
+                most_hungry_elf = current_elf_food_needs;
+            }
+            current_elf_food_needs = 0;
+        }
+        else {
+            current_elf_food_needs += elf.parse::<i32>().unwrap()
+        }
+    } 
+
+    println!("Most hungry elf food needs : {most_hungry_elf}")
+}
